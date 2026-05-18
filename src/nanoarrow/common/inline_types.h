258b140f89b3f17f7834492555b8fdc5c40b6fae
Dewey Dunnington <dewey@dunnington.ca>
2026-05-17 21:54:55 -0500

    fix!: Ensure ArrowTypeString never returns NULL (#885)
    Previously an unknown type identifier would have resulted in a NULL; now
    a conspicuous-looking possibly helpful string is shown.
    
    Almost all internal usage of ArrowTypeString is use in Printf strings,
    for which gcc now displays a warning if the value could be NULL.
    
    This is a breaking change if anybody was relying on the null output here
    to detect an invalid type identifier.
    
    Closes #884.

--- src/nanoarrow/common/inline_types.h
+++ src/nanoarrow/common/inline_types.h
@@ -463,7 +463,7 @@ enum ArrowType {
 /// \brief Get a string value of an enum ArrowType value
 /// \ingroup nanoarrow-utils
 ///
-/// Returns NULL for invalid values for type
+/// Returns "<unknown type identifier>" for invalid values for type
 static inline const char* ArrowTypeString(enum ArrowType type);
 
 static inline const char* ArrowTypeString(enum ArrowType type) {
@@ -559,7 +559,7 @@ static inline const char* ArrowTypeString(enum ArrowType type) {
     case NANOARROW_TYPE_LARGE_LIST_VIEW:
       return "large_list_view";
     default:
-      return NULL;
+      return "<unknown type identifier>";
   }
 }
 
